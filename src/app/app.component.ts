e5794741a460d51e12ccf37abac8adec4d25ea9a
nutv99 <nutv99@gmail.com>
2022-11-29 13:09:09 +0700

    3 files modified, 1 added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,53 @@
-import { Component, VERSION } from '@angular/core';
+import { Component, Pipe } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+// import { MyapiService } from './myapi.service';
+import { MyApiService } from './myapi.service';
+import { Subscription, Subject, switchMap, debounceTime, pipe } from 'rxjs';
 
 @Component({
   selector: 'my-app',
   templateUrl: './app.component.html',
-  styleUrls: [ './app.component.css' ]
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent  {
-  name = 'Angular ' + VERSION.major;
+export class AppComponent {
+  pokemons: any[] = [];
+  title = 'switchmap_pokemon';
+  subscription?: Subscription;
+
+  constructor(private http: HttpClient, private myapi: MyApiService) {
+    // this.onSearchPokemons.pipe(
+    //   //debounceTime(200),
+    //   switchMap(searchText =>{
+    //   //return this.myapiservice.getPokemonByName(searchText)
+    //   console.log('aaa' + searchText)
+    // })).subscribe(value=>console.log(value));
+  }
+
+  //  constructor(
+  //   private PokemonsKalosService : PokemonsKalosService
+  //   )
+  //    {
+
+  //      this.onSearchPokemons.pipe(
+  //        //debounceTime(200),
+  //        switchMap(searchText=>{
+  //        return this.PokemonsKalosService.getPokemonByName(searchText)
+  //      })).subscribe(value=>console.log(value));
+  //   }
+
+  onSearchPokemons = new Subject<string>();
+
+  searchPokemons(searchText: string) {
+    //  วิธีที่ 1
+    if (this.subscription) {
+      console.log('UnSubscribe');
+      this.subscription.unsubscribe();
+    }
+    this.subscription = this.myapi
+      .getPokemonByName(searchText)
+      .subscribe((response) => (this.pokemons = response));
+    //alert(searchText);
+    // วิธีที่ 2  switchMap
+    //this.onSearchPokemons.next(searchText);
+  }
 }
