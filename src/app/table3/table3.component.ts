40e6201926b35f9d2a15b15037637ce986830212
nutv99 <nutv99@gmail.com>
2022-11-30 08:21:45 +0700

    2 files modified

--- src/app/table3/table3.component.ts
+++ src/app/table3/table3.component.ts
@@ -1,11 +1,22 @@
 import { Component, OnInit } from '@angular/core';
 
+export interface ItemModel {
+  ItemID: number;
+  ItemName: string;
+  Description: string;
+  SellPrice: number;
+  NumOrder: number;
+  ThisMoney: number;
+}
+
 @Component({
   selector: 'app-table3',
   templateUrl: './table3.component.html',
   styleUrls: ['./table3.component.css'],
 })
 export class Table3Component implements OnInit {
+
+tblModel : ItemModel[] = [] ;  
   constructor() {}
 
   ngOnInit() {}
